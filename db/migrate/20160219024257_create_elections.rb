067aae9aedc32f0db82734f753afb9d0b912c8a2
America Calleros <america.quintero@gmail.com>
2016-02-18 21:30:30 -0600

    Add title, description and contact to election table

--- db/migrate/20160219024257_create_elections.rb
+++ db/migrate/20160219024257_create_elections.rb
@@ -1,8 +1,14 @@
 class CreateElections < ActiveRecord::Migration
-  def change
+  def up
     create_table :elections do |t|
-
-      t.timestamps null: false
+	t.string :title, null: false
+	t.text :description
+	t.string :contact, null: false
+	t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :elections
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,11 @@
 ActiveRecord::Schema.define(version: 20160219024257) do
 
   create_table "elections", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",       limit: 255,   null: false
+    t.text     "description", limit: 65535
+    t.string   "contact",     limit: 255,   null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
