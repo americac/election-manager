3b72bbe8b6bcf5d4b21fa3fc75a5d521105e4010
America Calleros <america.quintero@gmail.com>
2016-02-20 12:16:44 -0600

    Test validation of title, contact for election model

--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Election, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validate presence of required fields" do
+    it "validates presence of :title" do
+	expect(subject).to validate_presence_of(:title)
+    end
+    it "validates presence of :contact" do
+	expect(subject).to validate_presence_of(:contact)
+    end
+    xit "validates presence of :start" do
+      expect(subject).to validate_presence_of(:start)
+    end
+
+    xit "validates presence of :finish" do
+       expect(subject).to validate_presence_of(:finish)
+    end
+  end
+
+  xit "validates :conact to be an email address"
+    expect{raise StandardError, 'Contact is invalid'}.to raise_error('Contact is invalid')
+  end
 end
