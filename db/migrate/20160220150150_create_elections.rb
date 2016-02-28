8efdcf5d38fa52c302942aabe0a36e4a17625274
America Calleros <america.quintero@gmail.com>
2016-02-28 12:01:50 -0600

    Make title and contact not nullable

--- db/migrate/20160220150150_create_elections.rb
+++ db/migrate/20160220150150_create_elections.rb
@@ -1,9 +1,9 @@
 class CreateElections < ActiveRecord::Migration
   def change
     create_table :elections do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
-      t.string :contact
+      t.string :contact, null: false
 
       t.timestamps null: false
     end
