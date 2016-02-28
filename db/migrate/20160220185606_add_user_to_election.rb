2f4bdefe105f19d82981852268031c3a18f92b2b
America Calleros <america.quintero@gmail.com>
2016-02-28 10:49:31 -0600

    Add comment about user_id

--- db/migrate/20160220185606_add_user_to_election.rb
+++ db/migrate/20160220185606_add_user_to_election.rb
@@ -1,5 +1,6 @@
 class AddUserToElection < ActiveRecord::Migration
   def change
+  	#this creates a user_id in the elections table, used to keep track of owner of the election
     add_reference :elections, :user, null: false, index: true, foreign_key: true
 #    add_foreign_key :elections, :users
   end
