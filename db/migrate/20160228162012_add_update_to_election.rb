6393e7efb3beca93aab80dd54a7564a28e4d801f
America Calleros <america.quintero@gmail.com>
2016-03-03 13:34:22 -0600

    Add default value for elections update_by

--- db/migrate/20160228162012_add_update_to_election.rb
+++ db/migrate/20160228162012_add_update_to_election.rb
@@ -1,5 +1,5 @@
 class AddUpdateToElection < ActiveRecord::Migration
   def change
-    add_column :elections, :update_by, :string, null: false
+    add_column :elections, :update_by, :string, null: false, default: 'nobody'
   end
 end
