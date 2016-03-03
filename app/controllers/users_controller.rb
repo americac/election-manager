854dcbbaf4c76c60a52b90eb30aca68c26501474
America Calleros <america.quintero@gmail.com>
2016-03-03 14:58:09 -0600

    Add basic params for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,16 @@ class UsersController < ApplicationController
     end
     render "new"
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:lname, :fname, :netid, :global_admin)
+    end
+
 end
