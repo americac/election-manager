9bbdcd73b933f847112b0e53efaf6fd8589839c4
America Calleros <america.quintero@gmail.com>
2016-03-03 13:35:12 -0600

    Add subject object creation for election

--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -15,10 +15,46 @@ RSpec.describe Election, type: :model do
     xit "validates presence of :finish" do
        expect(subject).to validate_presence_of(:finish)
     end
+
+    xit "validates usage of owner_id instead of user_id" do
+    end
   end
 
-  describe "validate :contact" do
-    xit "validates :contact to be an email address" do
+  describe "validate election object creation" do
+    subject {
+      Election.create(
+	user_id: 2,
+	update_by: 'pjackson',
+	title: 'Annabeths Election in test',
+	description: 'New Election for testing for Annabeth',
+	contact: 'grover@uic.edu',
+      )
+    }
+
+    xit "validates onwer_id is set" do
+      expect(subject.owner_id).to eq(2)
+    end
+
+    it "validates update_by is set" do
+      expect(subject.update_by).to eq('pjackson')
+    end
+
+    it "validates title is set" do
+      expect(subject.title).to eq('Annabeths Election in test')
+    end
+
+    it "validates description is set" do
+      expect(subject.description).to eq('New Election for testing for Annabeth')
+    end
+
+    it "validates contact is set" do
+      expect(subject.contact).to eq('grover@uic.edu')
+    end
+
+  end
+
+  describe "validate contact:" do
+    it "validates contact: to be an email address" do
       subject.contact = "americauic.edu"
       expect{raise StandardError, 'Contact is invalid'}.to raise_error('Contact is invalid')
     end
