9aeaa590b0d6f253385d73781a3fafd9c15f1527
America Calleros <america.quintero@gmail.com>
2016-03-04 16:56:29 -0600

    Add testing of election model for start and finish

--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -8,57 +8,64 @@ RSpec.describe Election, type: :model do
     end
   end
   describe "validate presence of required fields" do
+      election = FactoryGirl.build(:election)
     it "validates presence of :title" do
-      election = FactoryGirl.create(:election)
       expect(election).to validate_presence_of(:title)
     end
     it "validates presence of :contact" do
-      election = FactoryGirl.create(:election)
       expect(election).to validate_presence_of(:contact)
     end
-    xit "validates presence of :start" do
-      election = FactoryGirl.create(:election)
+
+    it "validates presence of :start" do
       expect(election).to validate_presence_of(:start)
     end
 
-    xit "validates presence of :finish" do
-      election = FactoryGirl.create(:election)
+    it "validates presence of :finish" do
       expect(election).to validate_presence_of(:finish)
     end
 
-    xit "validates usage of owner_id instead of user_id" do
+    it "validates usage of owner_id instead of user_id" do
+      expect(election.owner_id).to be_truthy
     end
   end
 
   describe "validate election object creation" do
     election = FactoryGirl.create(:election)
-    xit "validates onwer_id is set" do
-      expect(election.owner_id).to be_valid
+    it "validates onwer_id is set" do
+      expect(election.owner_id).to be_truthy
     end
 
     it "validates update_by is set" do
-      expect(election.update_by).to be_valid
+      expect(election.update_by).to_not be_nil
     end
 
     it "validates title is set" do
-      expect(subject.title).to eq('Annabeths Election in test')
+      expect(election.title).to_not be_nil
     end
 
     it "validates description is set" do
-      expect(subject.description).to eq('New Election for testing for Annabeth')
+      expect(election.description).to_not be_nil
     end
 
     it "validates contact is set" do
-      expect(subject.contact).to eq('grover@uic.edu')
+      expect(election.contact).to_not be_nil
+    end
+
+    it "validates start is set" do
+      expect(election.start).to_not be_nil
+    end
+
+    it "validates finish is set" do
+      expect(election.finish).to_not be_nil
     end
 
   end
 
-  describe "validate contact:" do
-    it "validates contact: to be an email address" do
-      subject.contact = "americauic.edu"
-      expect{raise StandardError, 'Contact is invalid'}.to raise_error('Contact is invalid')
+  describe "validate object methods" do
+    xit "ensure start is not in the past" do
+      election = FactoryGirl.create(:election)
+      expect(election.start = Faker::Date.backward(1)).to raise_error
+      #expect{election.start = Faker::Date.backward(1)}.to raise_error(Election,"Start cannot be in the past")
     end
   end
-
 end
