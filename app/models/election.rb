d142aa3178aafbc80f3af72ea2d30dc590744519
America Calleros <america.quintero@gmail.com>
2016-03-07 15:13:04 -0600

    Make sure start is not after finish

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,9 +1,9 @@
 class Election < ActiveRecord::Base
-require 'date'
+  require 'date'
 #  attr_accessible :contact, :description, :title
 
   validate :validate_start_not_in_past
-  #validate :validate_start_not_in_past, on :update
+  validate :validate_start_not_before_finish
   validates :title, :presence => true
   validates :contact, :presence => true
   validates :start, :presence => true
@@ -17,10 +17,11 @@ require 'date'
 
 
   private
-    def validate_start_not_in_past
-      if start.present? && start<Date.today
-	errors.add(:election, "Start cannot be in the past")
-      end
+    def validate_start_not_before_finish
+      self.errors.add(:start, "cannot be before finish") if start.to_i > finish.to_i
     end
 
+    def validate_start_not_in_past
+      self.errors.add(:start, "cannot be in the past") if start.present? && start.past?
+    end
 end
