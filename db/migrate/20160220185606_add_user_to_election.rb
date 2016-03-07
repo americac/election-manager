0946c68e19602afd1531edcf900b42b453fd5827
America Calleros <america.quintero@gmail.com>
2016-03-07 16:34:00 -0600

    Remove commented out line

--- db/migrate/20160220185606_add_user_to_election.rb
+++ db/migrate/20160220185606_add_user_to_election.rb
@@ -2,6 +2,5 @@ class AddUserToElection < ActiveRecord::Migration
   def change
   	#this creates a user_id in the elections table, used to keep track of owner of the election
     add_reference :elections, :user, null: false, index: true, foreign_key: true
-#    add_foreign_key :elections, :users
   end
 end
